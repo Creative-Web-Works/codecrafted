273f1f58ef29de82e2102da3268fd975d5878b4f
CWWS <geemer@hotmail.co.uk>
2023-06-30 22:55:52 +0100

    bump-regex with semver

--- assets/js/material-kit-pro.js
+++ assets/js/material-kit-pro.js
@@ -812,4 +812,10 @@ function copyCode(el) {
   }
 }
 import stable from 'semver-stable';
-// End copy code function
+
+ 
+var bump = require('bump-regex');
+ 
+bump('version: "0.1.2"', function(err, out) {
+  // => 'version: "0.1.3"'
+});
